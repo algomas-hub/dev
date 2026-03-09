c9739c3a6fd6ffcf03d5f844443c04b35b590ee3
Alessio <alessio.rizzari@gmail.com>
2026-03-09 15:10:20 +0100

    Fix accessibility issues: use Link component and button elements instead of anchor tags

--- frontend/src/Nav/Nav.js
+++ frontend/src/Nav/Nav.js
@@ -1,35 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 
 function Nav(){
 return(
     <nav id="nv" className="navbar navbar-expand-md navbar-dark bg-dark">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <Link className="navbar-brand" to="/">Navbar</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="link">Link</a>
+                        <Link className="nav-link" to="/link">Link</Link>
                     </li>
                     <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a className="nav-link dropdown-toggle" href="#noop" onClick={(e) => e.preventDefault()} role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Dropdown
                         </a>
                         <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="#">Action</a></li>
-                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                            <li><button className="dropdown-item" type="button" style={{background: 'none', border: 'none', width: '100%', textAlign: 'left'}}>Action</button></li>
+                            <li><button className="dropdown-item" type="button" style={{background: 'none', border: 'none', width: '100%', textAlign: 'left'}}>Another action</button></li>
                            
-                            <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            <li><button className="dropdown-item" type="button" style={{background: 'none', border: 'none', width: '100%', textAlign: 'left'}}>Something else here</button></li>
                         </ul>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                        <span className="nav-link disabled" aria-disabled="true">Disabled</span>
                     </li>
                 </ul>
                 <form className="d-flex" role="search">
